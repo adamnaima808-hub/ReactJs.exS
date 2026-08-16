f67725fcfa8173c7e1ac87c47e4193f54a6dec93
adamnaima808-... <adamnaima808@gmail.com>
2026-08-16 10:54:58 +0300

    Fix subfolders git tracking and add files

---
+++ Exercises/exercise2/src/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2026 My Blog</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ Exercises/exercise2/src/Post.jsx
@@ -0,0 +1,10 @@
+const Post = () => {
+  return (
+    <article>
+      <h2>Post Title</h2>
+      <p>This is the content of the blog post.</p>
+    </article>
+  );
+};
+
+export default Post;
